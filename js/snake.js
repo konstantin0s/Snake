feba5262360235edaa7758bc4fd80ab3672718df
konstantin0s <constantintofan85@gmail>
2018-03-12 20:57:07 +0100

    check collision

--- js/snake.js
+++ js/snake.js
@@ -93,3 +93,43 @@ var Snake = function() {
 };
 
 //Draw a square for each segment of the snake's body
+Snake.prototype.draw = function() {
+  for (var i = 0; i < this.segments.length; i++) {
+    this.segments[i].drawSquare("Blue");
+  }
+};
+
+//Create a new head and add it to the beginning of the
+//snake to move the snake in it's current direction
+ Snake.prototype.move = function() {
+   var head = this.segments[0];
+   var newHead;
+
+   this.direction = this.nextDirection;
+
+   if(this.direction === "right") {
+     newHead = newBlock(head.col + 1, head.row);
+   } else if (this.direction === "down") {
+     newHead = new Block(head.col, head.row + 1);
+   } else if (this.direction === "left") {
+     newHead = new Block(head.col -1, head.row);
+   } else if (this.direction === "up") {
+     newHead = new Block(head.col, head.row -1);
+   }
+
+   if (this.checkCollision(newHead)) {
+     gameOver();
+     return;
+   }
+
+   this.segments.unshift(newHead);
+
+   if(newHead.equal(apple.position)) {
+     score++;
+     apple.move();
+   } else {
+     this.segments.pop();
+   }
+ };
+
+ 
